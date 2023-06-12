40bfe6c33d45c1613d2137820aff8b84f21a2873
ContinuedOak <118535044+ContinuedOak@users.noreply.github.com>
2023-06-13 09:33:31 +1000

    v2

--- DayNightCycle.cs
+++ DayNightCycle.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    public float dayLengthInMinutes = 1f;
-    public float rotationSpeed = 1f;
+     [Header("Day & Night Cycle")]
+    [Tooltip("Can the Time Pass?")] public bool doDayCycle;
+    public bool isNight;
+
+    [Tooltip("How long the in-game time is in real world minutes (Default = 15f)")] public float dayLengthInMinutes = 1f;
+    [Tooltip("How fast the sun rotates")] public float rotationSpeed = 1f;
     public float currentRotation = 0f;
-    public float currentDay = 1f;
+    [Tooltip("Current Day Counter (Default Start = 0)")] public float currentDay = 0f;
 
     void Update()
     {
-        float anglePerFrame = Time.deltaTime * (360f / (dayLengthInMinutes * 60f));
-        transform.Rotate(Vector3.right, anglePerFrame * rotationSpeed);
-        currentRotation += anglePerFrame * rotationSpeed;
-        if (currentRotation >= 360f)
+        if (doDayCycle)
         {
-            currentRotation -= 360f;
-            currentDay++;
+            float anglePerFrame = Time.deltaTime * (360f / (dayLengthInMinutes * 60f));
+            transform.Rotate(Vector3.right, anglePerFrame * rotationSpeed);
+            currentRotation += anglePerFrame * rotationSpeed;
+            if (currentRotation >= 360f)
+            {
+                currentRotation -= 360f;
+                currentDay++;
+            }
+
+
+            // Turns off Lights if Night Time
+
+
+            if (currentRotation >= 133f && currentRotation <= 312f)
+            {
+                isNight = true;
+            }
+            else
+            {
+                isNight = false;
+            }
+
+            Light light = GetComponent<Light>();
+            light.enabled = !isNight;
         }
     }
 }
